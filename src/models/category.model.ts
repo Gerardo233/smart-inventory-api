4d4563d7dbd47d7516e9b8bdfc1d0dd3ab275627
Gerardo233 <miguelmr1107@gmail.com>
2025-07-29 20:38:36 -0600

    :rocket: Adding category model

---
+++ src/models/category.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model, Document } from "mongoose";
+
+interface ICategory extends Document {
+  name: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const CategorySchema = new Schema<ICategory>({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const CategoryModel = model<ICategory>("Category", CategorySchema);
+
+export default CategoryModel;
